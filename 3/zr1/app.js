416ec1bedff9e2c36d13e3be74dee551b5aad1cf
Denislav Volin-Danylov <netranner22845@gmail.com>
2025-12-18 19:29:38 +0200

    add log system and fix ligth mode

--- 3/zr1/app.js
+++ 3/zr1/app.js
@@ -1,16 +1,19 @@
 
-const targetTempSlider = document.getElementById('target-temp-slider');
-const targetTempLabel = document.getElementById('target-temp-val');
-const targetHumidSlider = document.getElementById('target-humid-slider');
-const targetHumidLabel = document.getElementById('target-humid-val');
 const outsideTempSlider = document.getElementById('outside-temp-slider');
+const targetHumidSlider = document.getElementById('target-humid-slider');
+const targetTempSlider = document.getElementById('target-temp-slider');
+const targetHumidLabel = document.getElementById('target-humid-val');
 const outsideTempLabel = document.getElementById('outside-temp-val');
+const targetTempLabel = document.getElementById('target-temp-val');
 const detailRoomName = document.getElementById('detail-room-name');
-const detailTemp = document.getElementById('detail-temp');
 const detailHumidity = document.getElementById('detail-humidity');
-const houseGrid = document.getElementById('house-grid');
 const Select = document.getElementById('day-night-select');
+const detailTemp = document.getElementById('detail-temp');
+const logArea = document.getElementById('log-container');
+const houseGrid = document.getElementById('house-grid');
 const roomElements = document.querySelectorAll('.room');
+
+const lastStates = {};
 
 let activeRoomId = 'living';
 
@@ -40,13 +43,15 @@ Select.addEventListener('input', function() {
     if (Select.value == 'day') {
         isNight = false;
         houseGrid.style.backgroundColor = '#fff5d7';
+        printLog("Перемикання на денний режим");
     }
     else if (Select.value == 'night') {
         isNight = true;
         houseGrid.style.backgroundColor = '#252530';
+        printLog("Перемикання на нічний режим");
     }
     else{
-        alert("Помиклка вибору часу");
+        printLog("Помиклка вибору часу");
     }
 })
 
@@ -63,7 +68,7 @@ function selectRoom(roomId) {
     const roomData = houseState.find(room => room.id === roomId);
 
     if (!roomData) {
-        console.error("Кімнату не знайдено в даних!");
+        printLog("Кімнату не знайдено в даних!");
         return;
     }
 
@@ -76,7 +81,6 @@ function selectRoom(roomId) {
 
     targetHumidSlider.value = roomData.targetHumidity;
     targetHumidLabel.innerText = roomData.targetHumidity;
-
     updateDeviceStatus(roomData);
 }
 
@@ -93,7 +97,6 @@ function updateDeviceStatus(roomData) {
             }
         }
     };
-
     setStatus('dev-ac', roomData.acON);
     setStatus('dev-heater', roomData.heaterON);
     setStatus('dev-humid', roomData.humidifierON);
@@ -101,12 +104,20 @@ function updateDeviceStatus(roomData) {
 }
 
 function updateRoomLigth(roomData) {
-    const room = document.getElementById('room-' + roomData.id)
+    const room = document.getElementById('room-' + roomData.id);
+    const roomIcons = room.querySelector('.room-icons');
+    const roomTitle = room.querySelector('.room-title');    
+
+
     if(roomData.ligthON) {
         room.classList.add('ligth');
+        roomIcons.classList.add('ligth-font');
+        roomTitle.classList.add('ligth-font');
     }
     else {
         room.classList.remove('ligth');
+        roomIcons.classList.remove('ligth-font');
+        roomTitle.classList.remove('ligth-font');
     }
 }
 
@@ -116,13 +127,9 @@ function updateRoomVisuals(roomId, temp, humid) {
     const humidSpan = roomDiv.querySelector('#humid');
 
     const roomData = houseState.find(r => r.id === roomId);
-    if (roomData) {
+    if (roomData) 
         updateRoomLigth(roomData);
         
-        if (activeRoomId === roomId) {
-             updateDeviceStatus(roomData); 
-        }
-    }
 
     tempSpan.innerText = `🌡️ ${temp}°`;
     humidSpan.innerText = `💧 ${humid}%`;
@@ -130,8 +137,55 @@ function updateRoomVisuals(roomId, temp, humid) {
 
 }
 
+function checkStateChanges(room) {
+    if (!lastStates[room.id]) {
+        lastStates[room.id] = {
+            light: room.ligthON,
+            heater: room.heaterON,
+            ac: room.acON,
+            humidifier: room.humidifierON
+        };
+        return;
+    }
+
+    const last = lastStates[room.id];
+
+    if (room.ligthON !== last.light) {
+        const status = room.ligthON ? "УВІМКНЕНО" : "ВИМКНЕНО";
+        printLog(`${room.name}: Світло ${status}`);
+        last.light = room.ligthON;
+    }
+    if (room.heaterON !== last.heater) {
+        const status = room.heaterON ? "Увімкнено обігрів 🔥" : "Вимкнено обігрів";
+        printLog(`${room.name}: ${status}`);
+        last.heater = room.heaterON;
+    }
+
+    if (room.acON !== last.ac) {
+        const status = room.acON ? "Увімкнено охолодження ❄️" : "Вимкнено охолодження";
+        printLog(`${room.name}: ${status}`);
+        last.ac = room.acON;
+    }
+
+    if (room.humidifierON !== last.humidifier) {
+        const status = room.humidifierON ? "Увімкнено зволоження 💧" : "Вимкнено зволоження";
+        printLog(`${room.name}: ${status}`);
+        last.humidifier = room.humidifierON;
+    }
+}
+
+function printLog(message) {
+    const timestamp = new Date().toLocaleTimeString();
+    let messageText  = `[${timestamp}] ${message}\n`;
+    const logEntry = document.createElement('div');
+    logEntry.className = 'log-entry';
+    logEntry.innerText = messageText;
+    logArea.appendChild(logEntry);
+    logArea.scrollTop = logArea.scrollHeight;
+}
+
 function startSimulation() {
-    
+    printLog('Симуляція розпочато.');
     const startBtn = document.getElementById('start-sim-btn');
     startBtn.disabled = true;
     startBtn.textContent = 'Симуляція...';
@@ -141,9 +195,10 @@ function startSimulation() {
     setInterval(() => {
         houseState.forEach(room => {
            updateIndicators(room);
+           checkStateChanges(room);
         });
     }, 1000);
-
 }
+
 
 
